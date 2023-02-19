bd844c7f46d0dcd8bcb0f38d6951c598deac23a9
raschaZ <rachazaibi27051997@gmail.com>
2023-02-19 01:28:22 +0100

    fade in-out annimation

--- animation_css/src/App.vue
+++ animation_css/src/App.vue
@@ -1,10 +1,32 @@
-<template></template>
+<template>
+  <button type="button" @click="flag= !flag">Toggle</button>
+  <transition name="fade" mode="out-in">
+    <h2 v-if="flag" key="main">Hello World!</h2>
+    <h2 v-else="flag" key="secondary">Hi World!</h2>
+  </transition>
+</template>
 
 <script>
 export default {
-  name: "App"
+  name: "App",
+  data() {
+    return {
+      flag : false ,
+    }
+  },
 }
 </script>
 
 <style>
+.fade-enter-from{
+  opacity : 0 ;
+}
+.fade-enter-active {
+  transition: all 3s linear;
+}
+
+.fade-leave-to {
+  transition: all 3s linear;
+  opacity: 0;
+}
 </style>
