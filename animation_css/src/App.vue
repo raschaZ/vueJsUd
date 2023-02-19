293597f640063fa8c6ddc92c24f10e0940c4f779
raschaZ <rachazaibi27051997@gmail.com>
2023-02-19 01:45:03 +0100

    zoom type and appearence

--- animation_css/src/App.vue
+++ animation_css/src/App.vue
@@ -4,6 +4,10 @@
     <h2 v-if="flag" key="main">Hello World!</h2>
     <h2 v-else="flag" key="secondary">Hi World!</h2>
   </transition>
+  <transition name="zoom" type="animation" appear>
+    <h2 v-if="flag" key="main">Hello World!</h2>
+    <h2 v-else="flag" key="secondary">Hi World!</h2>
+  </transition>
 </template>
 
 <script>
@@ -11,7 +15,7 @@ export default {
   name: "App",
   data() {
     return {
-      flag : false ,
+      flag : true ,
     }
   },
 }
@@ -22,11 +26,52 @@ export default {
   opacity : 0 ;
 }
 .fade-enter-active {
-  transition: all 3s linear;
+  transition: all 0.25s linear;
 }
 
 .fade-leave-to {
-  transition: all 3s linear;
+  transition: all 0.25s linear;
   opacity: 0;
 }
+
+h2{
+  width: 400px;
+  padding: 20px;
+  margin: 20px;
+}
+.zoom-enter-active{
+ animation:zoom-in 1s linear forwards;
+ transition: all 2s linear;
+}
+
+.zoom-leave-active {
+  animation:zoom-out 1s linear forwards;
+  transition: all 2s linear;
+}
+
+.zoom-enter-from{
+  opacity : 0 ;
+}
+
+.zoom-leave-to {
+  opacity: 0 ;
+}
+
+@keyframes zoom-in {
+  from{
+    transform: scale(0,0);
+  }
+  to{
+    transform: scale(1,1);
+  }
+}
+@keyframes zoom-out{
+  from{
+    transform: scale(1,1);
+  }
+  to{
+    transform: scale(0,0);
+  }
+}
+
 </style>
